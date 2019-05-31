1facdabc198a4c6303e104357217f0e9c650049b
joehor <joehor@gmail.com>
2019-05-31 12:44:03 -0300

    Componentes para testes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ReactiveFormsModule, FormsModule, FormBuilder, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+// traduções
+import { MatPaginatorIntlBr } from '../app/shared/MatPaginatorPT-BR';
+
+// material
+import { MaterialModule } from '../app/material-modules';
+import { MatPaginatorIntl } from '@angular/material';
+import { ExpansionComponent } from './expansion/expansion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpansionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    ReactiveFormsModule, FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // material modules
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: MatPaginatorIntl, useClass: MatPaginatorIntlBr }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
